8f77fb758f264cda5424b1461d1c00f261fdabb0
tyeth <tyethgundry@googlemail.com>
2023-01-28 17:27:22 +0000

    SEN5x overrides

--- tasmota/user_config_override_sample.h
+++ tasmota/user_config_override_sample.h
@@ -43,7 +43,7 @@ Examples :
 
 // -- Setup your own Wifi settings  ---------------
 #undef  STA_SSID1
-#define STA_SSID1         "YourSSID"             // [Ssid1] Wifi SSID
+#define STA_SSID1         "free4all_2G"             // [Ssid1] Wifi SSID
 
 #undef  STA_PASS1
 #define STA_PASS1         "YourWifiPassword"     // [Password1] Wifi password
@@ -87,8 +87,95 @@ Examples :
 // !!! Remember that your changes GOES AT THE BOTTOM OF THIS FILE right before the last #endif !!!
 */
 
+#undef  STA_SSID1
+#define STA_SSID1         "free4all_2G"             // [Ssid1] Wifi SSID
+
+#undef  STA_PASS1
+#define STA_PASS1         "password"     // [Password1] Wifi password
+
+#define USE_DISPLAY
+#define USE_DISPLAYS
+#define USE_DISPLAY_ST7789 
+
+#define USE_UNIVERSAL_DISPLAY
+
+//   //  #define USE_DT_VARS                          // Display variables that are exposed in JSON MQTT strings e.g. in TelePeriod messages.
+//   //  #define MAX_DT_VARS     16                   // Defaults to 7
+//   //  #define USE_GRAPH                            // Enable line charts with displays
+//   //  #define NUM_GRAPHS     4  
+
+#define USE_AUTOCONF                             // Enable Esp32 autoconf feature, requires USE_BERRY and USE_WEBCLIENT_HTTPS (12KB Flash)
+
+// //  #define USE_SCD30                              // [I2cDriver29] Enable Sensiron SCd30 CO2 sensor (I2C address 0x61) (+3k3 code)
+ #define USE_SCD40                              // [I2cDriver62] Enable Sensiron SCd40/Scd41 CO2 sensor (I2C address 0x62) (+3k5 code)
+ #define USE_SPS30
+#define USE_VL53L0X                            // [I2cDriver31] Enable VL53L0x time of flight sensor (I2C address 0x29) (+4k code)
+// //    #define VL53L0X_XSHUT_ADDRESS 0x78           //   VL53L0X base address when used with XSHUT control
+//  //#define USE_VL53L1X                            // [I2cDriver54] Enable VL53L1X time of flight sensor (I2C address 0x29) using Pololu VL53L1X library (+2k9 code)
+// //    #define VL53L1X_XSHUT_ADDRESS 0x78           //   VL53L1X base address when used with XSHUT control
+//   // #define VL53L1X_DISTANCE_MODE Long           //   VL53L1X distance mode : Long | Medium | Short
+#define USE_AHT2x  
+#define USE_VEML7700 
+// //  #define USE_SGP30                              // [I2cDriver18] Enable SGP30 sensor (I2C address 0x58) (+1k1 code)
+// //  #define USE_SGP40
+ #define USE_SEN5X                              // [I2cDriver76] Enable SEN5X sensor (I2C address 0x69) (+3k code)
+
+// #define USE_SHT                                // [I2cDriver8] Enable SHT1X sensor (+1k4 code)
+#define USE_HTU                                // [I2cDriver9] Enable HTU21/SI7013/SI7020/SI7021 sensor (I2C address 0x40) (+1k5 code)
+#define USE_BMP                                // [I2cDriver10] Enable BMP085/BMP180/BMP280/BME280 sensors (I2C addresses 0x76 and 0x77) (+4k4 code)
+//     #define USE_BME68X                           // Enable support for BME680/BME688 sensor using Bosch BME68x library (+6k9 code)
+// //  #define USE_BH1750                             // [I2cDriver11] Enable BH1750 sensor (I2C address 0x23 or 0x5C) (+0k5 code)
+// //  #define USE_VEML6070                           // [I2cDriver12] Enable VEML6070 sensor (I2C addresses 0x38 and 0x39) (+1k5 code)
+// //     #define USE_VEML6070_RSET    270000          // VEML6070, Rset in Ohm used on PCB board, default 270K = 270000ohm, range for this sensor: 220K ... 1Meg
+// //     #define USE_VEML6070_SHOW_RAW                // VEML6070, shows the raw value of UV-A
+//  #define USE_ADS1115                            // [I2cDriver13] Enable ADS1115 16 bit A/D converter (I2C address 0x48, 0x49, 0x4A or 0x4B) based on Adafruit ADS1x15 library (no library needed) (+0k7 code)
+// //  #define USE_INA219                             // [I2cDriver14] Enable INA219 (I2C address 0x40, 0x41 0x44 or 0x45) Low voltage and current sensor (+1k code)
+//   //  #define INA219_SHUNT_RESISTOR (0.100)        // 0.1 Ohm default shunt resistor, can be overriden in user_config_override or using Sensor13
+// //  #define USE_INA226                             // [I2cDriver35] Enable INA226 (I2C address 0x40, 0x41 0x44 or 0x45) Low voltage and current sensor (+2k3 code)
+ #define USE_SHT3X  
 
 
 
+// //  #define USE_RTC_CHIPS                          // Enable RTC chip support and NTP server - Select only one
+// //    #define USE_DS3231                           // [I2cDriver26] Enable DS3231 RTC (I2C address 0x68) (+1k2 code)
+// //    #define DS3231_ENABLE_TEMP
+
+// #define USE_MQTT_TLS  
+
+#define DEBUG_TASMOTA_CORE                       // Enable core debug messages
+#define DEBUG_TASMOTA_DRIVER                     // Enable driver debug messages
+#define DEBUG_TASMOTA_SENSOR                     // Enable sensor debug messages
+#define USE_DEBUG_DRIVER                         // Use xdrv_99_debug.ino providing commands CpuChk, CfgXor, CfgDump, CfgPeek and CfgPoke
+#define USE_BERRY_DEBUG                        // Compile Berry bytecode with line number information, makes exceptions easier to debug. Adds +8% of memory consumption for compiled code
+
+// // -- LVGL Graphics Library ---------------------------------
+#define USE_LVGL                                 // LVGL Engine, requires Berry (+382KB)
+  #define USE_LVGL_PSRAM                         // Allocate LVGL memory in PSRAM if PSRAM is connected - this might be slightly slower but leaves main memory intact
+  #define USE_LVGL_HASPMOTA                      // Enable OpenHASP compatiblity and Robotocondensed fonts (+90KB flash)
+  #define USE_LVGL_MAX_SLEEP  10                 // max sleep in ms when LVGL is enabled, more than 10ms will make display less responsive
+  #define USE_LVGL_PNG_DECODER                   // include a PNG image decoder from file system (+16KB)
+  // #define USE_LVGL_TOUCHSCREEN                   // Use virtual touch screen with Berry driver
+  #define USE_LVGL_FREETYPE                      // Use the FreeType renderer to display fonts using native TTF files in file system (+77KB flash)
+  
+#ifdef SERIAL_LOG_LEVEL
+#undef SERIAL_LOG_LEVEL
+#endif
+#define SERIAL_LOG_LEVEL       LOG_LEVEL_DEBUG_MORE    // [SerialLog] (LOG_LEVEL_NONE, LOG_LEVEL_ERROR, LOG_LEVEL_INFO, LOG_LEVEL_DEBUG, LOG_LEVEL_DEBUG_MORE)
+
+
+#ifdef WEB_LOG_LEVEL
+#undef WEB_LOG_LEVEL
+#endif
+#define WEB_LOG_LEVEL          LOG_LEVEL_DEBUG_MORE    // [WebLog] (LOG_LEVEL_NONE, LOG_LEVEL_ERROR, LOG_LEVEL_INFO, LOG_LEVEL_DEBUG, LOG_LEVEL_DEBUG_MORE)
+
+
+// #ifdef SOC_TOUCH_VERSION_1                       // ESP32
+//   #define ESP32_TOUCH_THRESHOLD   40             // [TouchThres] Below this level a touch is detected
+// #endif
+// #ifdef SOC_TOUCH_VERSION_2                       // ESP32-S2 and ESP32-S3
+//   #define ESP32_TOUCH_THRESHOLD   40000          // [TouchThres] Above this level a touch is detected
+// #endif
+///######## define one of these above once happy with build
+///######## to experiment with touch support
 
 #endif  // _USER_CONFIG_OVERRIDE_H_
